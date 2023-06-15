c21aa40ee0b05873a0d5ba8ead503f28f6bd59d0
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-06-15 14:58:54 +0700

    express

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const http = require("http");
-// const express = require("express");
+const express = require("express");
 const fs = require("fs");
 
-// const app = express();
+const app = express();
 const title = fs.readFileSync('req-path/input.txt',"utf-8")
 
-const server = http.createServer((req,res)=>{
-    const path = req.url
-    console.log("url = ",path)
-    if (path ==='/' || path ==='home'){
-        const html=`
-        <h1>Home</h1>
-        <h4>${title}</h4>
-        <p>Welcome to  Homepage</p>
-        `
-        res.end(html)
-    }else if (path === '/phol'){
-        res.end('<h1>Passapol Sutatam</h1>')
-    }else{
-        res.writeHead(404)
-        res.end("<h1>Not found</h1>")
-    }
+app.use((req,res)=>{
+    res.send("hello world!");
 })
-server.listen(8080,'localhost',()=>{
-    console.log("/////////////////////////////////")
-    console.log("/// start server in port 8080 ///")
-    console.log("/////////////////////////////////")
-})
+
+// const server = http.createServer((req,res)=>{
+//     const path = req.url
+//     console.log("url = ",path)
+//     if (path ==='/' || path ==='home'){
+//         const html=`
+//         <h1>Home</h1>
+//         <h4>${title}</h4>
+//         <p>Welcome to  Homepage</p>
+//         `
+//         res.end(html)
+//     }else if (path === '/phol'){
+//         res.end('<h1>Passapol Sutatam</h1>')
+//     }else{
+//         res.writeHead(404)
+//         res.end("<h1>Not found</h1>")
+//     }
+// })
+// server.listen(8080,'localhost',()=>{
+//     console.log("/////////////////////////////////")
+//     console.log("/// start server in port 8080 ///")
+//     console.log("/////////////////////////////////")
+// })
