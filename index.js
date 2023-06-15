7f15c14b68f7c8ce4ac1d235aba5f444b0bad84f
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-06-15 14:51:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+// const express = require("express");
+const fs = require("fs");
+
+// const app = express();
+const title = fs.readFileSync('req-path/input.txt',"utf-8")
+
+const server = http.createServer((req,res)=>{
+    const path = req.url
+    console.log("url = ",path)
+    if (path ==='/' || path ==='home'){
+        const html=`
+        <h1>Home</h1>
+        <h4>${title}</h4>
+        <p>Welcome to  Homepage</p>
+        `
+        res.end(html)
+    }else if (path === '/phol'){
+        res.end('<h1>Passapol Sutatam</h1>')
+    }else{
+        res.writeHead(404)
+        res.end("<h1>Not found</h1>")
+    }
+})
+server.listen(8080,'localhost',()=>{
+    console.log("/////////////////////////////////")
+    console.log("/// start server in port 8080 ///")
+    console.log("/////////////////////////////////")
+})
